ffbea17f3e48d36c15cb4eac79f7e7a650012b21
MCrank <marco_mcse@hotmail.com>
2019-01-22 23:37:55 -0600

    READ campaigns working, WIP

---
+++ src/components/CampaignItem/CampaignItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './CampaignItem.scss';
+
+class CampaignItem extends React.Component {
+  render() {
+    return (
+      <div className="CampaignItem">
+        <h1>My Campaign Items</h1>
+      </div>
+    );
+  }
+}
+
+export default CampaignItem;
--- src/components/pages/MyCampaigns/MyCampaigns.js
+++ src/components/pages/MyCampaigns/MyCampaigns.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import authRequests from '../../../helpers/data/authRequests';
+import campaignRequests from '../../../helpers/data/campaignRequests';
+import CampaignItem from '../../CampaignItem/CampaignItem';
 
 class MyCampaigns extends React.Component {
+  state = {
+    campaigns: [],
+  };
+
+  componentDidMount() {
+    campaignRequests
+      .getMyCampaigns(authRequests.getCurrentUid())
+      .then((results) => {
+        this.setState({
+          campaigns: results,
+        });
+      })
+      .catch(error => console.error('An error occured retrieving campaigns', error));
+  }
+
   render() {
+    const { campaigns } = this.state;
+    const campaignItemComponent = campaignsArr => campaignsArr.map(campaign => <CampaignItem key={campaign.id} campaign={campaign} />);
+
     return (
       <div className="myCampaigns">
         <h1>MyCampaigns</h1>
+        {campaignItemComponent(campaigns)}
       </div>
     );
   }
