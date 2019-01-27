63456315e26d4bf04aaee10d02a4158d498f4e50
MCrank <marco_mcse@hotmail.com>
2019-01-26 22:52:25 -0600

    Autosuggest on home page working

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,96 @@
 import React from 'react';
+import { AsyncTypeahead } from 'react-bootstrap-typeahead';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import autoSuggest from '../../../helpers/data/autoSuggest';
+import './Home.scss';
 
 class Home extends React.Component {
+  state = {
+    isLoading: false,
+    autoFocus: true,
+    clearButton: true,
+    selectHintOnEnter: true,
+    suggestResults: [],
+    selectedAddress: [],
+    position: {
+      lat: 51.505,
+      lng: -0.09,
+    },
+    haveUsersLocation: false,
+    zoom: 2,
+    searchRadius: 24140,
+  };
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.setState({
+            position: {
+              lat: pos.coords.latitude,
+              lng: pos.coords.longitude,
+            },
+            haveUsersLocation: true,
+            zoom: 13,
+          });
+        },
+        () => {
+          autoSuggest
+            .getIpLocation()
+            .then((results) => {
+              this.setState({
+                position: {
+                  lat: results.data.latitude,
+                  lng: results.data.longitude,
+                },
+              });
+            })
+            .catch(error => console.error('There was an error getting IP location', error));
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 3000,
+        },
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser');
+    }
+  }
+
+  autoSuggestEvent = (e) => {
+    this.setState({ isLoading: true });
+    const query = e;
+    const { position } = this.state;
+    autoSuggest
+      .getAutoSuggest(query, position.lat, position.lng)
+      .then((results) => {
+        this.setState({
+          isLoading: false,
+          suggestResults: results,
+        });
+      })
+      .catch(error => console.error('There was an issue gettign autosuggest results', error));
+  };
+
   render() {
+    const {
+      suggestResults, isLoading, autoFocus, clearButton, selectHintOnEnter,
+    } = this.state;
     return (
       <div className="Home">
-        <h1>The Home Page</h1>
+        <h1 className="mt-2">Search for Campaigns</h1>
+        <AsyncTypeahead
+          autoFocus={autoFocus}
+          clearButton={clearButton}
+          className="suggest-input"
+          labelKey="formattedAddress"
+          options={suggestResults}
+          isLoading={isLoading}
+          selectHintOnEnter={selectHintOnEnter}
+          placeholder="Enter your address"
+          bsSize="lg"
+          onSearch={this.autoSuggestEvent}
+        />
       </div>
     );
   }
---
+++ src/helpers/data/autoSuggest.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys';
+
+const bingKey = apiKeys.bingMaps.key;
+const apiBaseUrl = 'https://dev.virtualearth.net/REST/v1/Autosuggest';
+
+// El%20bur&userLocation=47.668697,-122.376373,5&includeEntityTypes=Address,Place&key=<BingMapsKey>';
+
+const getAutoSuggest = (query, lat, lng) => new Promise((resolve, reject) => {
+  axios
+    .get(`${apiBaseUrl}?q=${query}&ul=${lat},${lng},5000&inclenttype=Address,Business&key=${bingKey}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((resp) => {
+      const results = resp.data.resourceSets[0].resources[0].value;
+      const formattedAddress = results.map(result => result.address.formattedAddress);
+      resolve(formattedAddress);
+    })
+    .catch(error => reject(error));
+});
+
+const getIpLocation = () => new Promise((resolve, reject) => {
+  axios
+    .get('https://ipapi.co/json')
+    .then((results) => {
+      resolve(results);
+    })
+    .catch(error => reject(error));
+});
+
+export default { getAutoSuggest, getIpLocation };
